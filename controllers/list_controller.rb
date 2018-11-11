3d001d604f20de5dbf98e1fb672ef57b5af0ac9f
Malcolm Finlayson <malcolmfinlayson81@gmail.com>
2018-11-11 12:58:52 +0000

    Work on add and list controllers

---
+++ controllers/list_controller.rb
@@ -0,0 +1,36 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative('controllers/add_controller')
+require_relative('controllers/list_controller')
+require_relative('controllers/show_controller')
+require_relative('controllers/category_controller')
+require_relative('controllers/budget_controller')
+also_reload( '../models/*' )
+
+get '/spending-tracker/list/all' do
+  @transactions = Transaction.all()
+  erb(:"list/all")
+end
+
+get '/spending-tracker/list/all/:id/edit' do
+  transactions_all = Transaction.all()
+  @to_edit = transactions_all.select { |transaction| transaction.id == params[:id] }
+  @others = transactions_all.select { |transaction| transaction.id != params[:id] }  }
+
+  @category_groups = CategoryGroup.all()
+  @categories = Category.all()
+  @time_periods = TimePeriod.all()
+  erb(:"list/edit")
+end
+
+post '/spending-tracker/list/all/:id' do
+  transaction = Transaction.new( params )
+  transaction.update()
+  redirect to ("/spending-tracker/list/all")
+end
+
+post '/spending-tracker/list/all/:id/delete' do
+  transaction = Transaction.delete( params[:id] )
+  redirect to ("/spending-tracker/list/all")
+end
