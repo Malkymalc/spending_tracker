c2bfd5ba37a4bd77b7050348d9532e32144dd584
Malcolm Finlayson <malcolmfinlayson81@gmail.com>
2018-11-13 11:45:39 +0000

    Some work on controllers done

--- controllers/categories_controller.rb
+++ controllers/categories_controller.rb
@@ -8,82 +8,44 @@ require_relative('../models/time_period.rb')
 require_relative('../models/transaction.rb')
 also_reload( '../models/*' )
 
-#CREATE
-get '/spending-tracker/category/new' do
+# index (Read)
+get '/spending-tracker/categories' do
+  @categories = Category.all()
   @category_groups = CategoryGroup.all()
-  erb(:"categories/add")
+  erb (:'categories/index')
 end
 
-get '/spending-tracker/category_group/new' do
-  @category_groups = CategoryGroup.all()
-  erb(:"category_group/add")
+# new (Create)
+get '/spending-tracker/categories/new' do
+  @categories = Category.all()
+  erb (:'categories/new')
 end
 
-post '/spending-tracker/category' do
+post '/spending-tracker/categories' do
   category = Category.new( params )
   category.save()
-  #confirmation page?
-  redirect to  ("categories/all")
+  redirect to '/spending-tracker/categories'
 end
 
-post '/spending-tracker/category_group' do
-  category_group = CategoryGroup.new( params )
-  category_group.save()
-  #confirmation page?
-  redirect to  ("categories/all")
-end
-
-# DELETE
-post '/spending-tracker/categories/all/:id/delete' do
-  Category.delete( params[:id] )
-  redirect to ("/spending-tracker/categories/all")
-end
-
-post '/spending-tracker/category_groups/all/:id/delete' do
-  CategoryGroup.delete( params[:id] )
-  redirect to ("/spending-tracker/categories/all")
-end
-
-
-#UPDATE
-get '/spending-tracker/categories/all/:id/edit' do
-  transactions_all = Transaction.all()
-  @to_edit = transactions_all.select { |transaction| transaction.id == params[:id] }
-  @others = transactions_all.select { |transaction| transaction.id != params[:id] }
-
-  @category_groups = CategoryGroup.all()
+# edit (Update)
+get '/spending-tracker/categories/:id/edit' do
+  @category = Category.find( params[:id] )
   @categories = Category.all()
-  @time_periods = TimePeriod.all()
-  erb(:"spending-tracker/categories/edit")
+  .select { |category|
+    category.id != @category.id
+  }
+  erb (:'categories/edit')
 end
 
-get '/spending-tracker/categories/all/:id/edit' do
-  transactions_all = Transaction.all()
-  @to_edit = transactions_all.select { |transaction| transaction.id == params[:id] }
-  @others = transactions_all.select { |transaction| transaction.id != params[:id] } 
-
-  @category_groups = CategoryGroup.all()
-  @categories = Category.all()
-  @time_periods = TimePeriod.all()
-  erb(:"spending-tracker/category-groups/edit")
+post '/spending-tracker/categories/:id' do
+  category = Category.new( params )
+  category.update()
+  redirect to '/spending-tracker/categories'
 end
 
-post '/spending-tracker/categories/all/:id' do
-  transaction = Transaction.new( params )
-  transaction.update()
-  redirect to ("/spending-tracker/categories/all")
-end
-
-post '/spending-tracker/category_groups/all/:id' do
-  category_group = CategoryGroup.new( params )
-  category_group.update()
-  redirect to ("/spending-tracker/categories/all")
-end
-
-
-
-#READ
-get '/spending-tracker/categories/all' do
-  @category_groups = CategoryGroup.all()
-  erb(:"categories/all")
+# delete (Delete)
+post '/spending-tracker/categories/:id/delete' do
+  category = Category.find( params[:id] )
+  category.delete()
+  redirect to 'spending-tracker/categories'
 end
--- controllers/time_periods_controller.rb
+++ controllers/time_periods_controller.rb
@@ -7,3 +7,55 @@ require_relative('../models/category_group.rb')
 require_relative('../models/time_period.rb')
 require_relative('../models/transaction.rb')
 also_reload( '../models/*' )
+
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative('../models/budget.rb')
+require_relative('../models/category.rb')
+require_relative('../models/category_group.rb')
+require_relative('../models/time_period.rb')
+require_relative('../models/transaction.rb')
+also_reload( '../models/*' )
+
+# index (Read)
+get '/spending-tracker/categories' do
+  @categories = Category.all()
+  @category_groups = CategoryGroup.all()
+  erb (:'categories/index')
+end
+
+# new (Create)
+get '/spending-tracker/categories/new' do
+  @categories = Category.all()
+  erb (:'categories/new')
+end
+
+post '/spending-tracker/categories' do
+  category = Category.new( params )
+  category.save()
+  redirect to '/spending-tracker/categories'
+end
+
+# edit (Update)
+get '/spending-tracker/categories/:id/edit' do
+  @category = Category.find( params[:id] )
+  @categories = Category.all()
+  .select { |category|
+    category.id != @category.id
+  }
+  erb (:'categories/edit')
+end
+
+post '/spending-tracker/categories/:id' do
+  category = Category.new( params )
+  category.update()
+  redirect to '/spending-tracker/categories'
+end
+
+# delete (Delete)
+post '/spending-tracker/categories/:id/delete' do
+  category = Category.find( params[:id] )
+  category.delete()
+  redirect to 'spending-tracker/categories'
+end
